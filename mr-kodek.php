8ad7884c9a82a4f36baa89ebcaf8c1126f307006
mr skk <suryanaepon7@gmail.com>
2025-09-07 17:30:05 +0700

    Update and rename kodek.php to mr-kodek.php

---
+++ mr-kodek.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hacked by MR KODEK</title>
+    <style>
+        body {
+            background-color: black;
+            color: red;
+            text-align: center;
+            font-family: 'Courier New', monospace;
+        }
+        h1 {
+            font-size: 50px;
+            text-shadow: 0 0 10px red;
+        }
+        p {
+            font-size: 20px;
+        }
+        .blink {
+            animation: blink 1s infinite;
+        }
+        @keyframes blink {
+            50% { opacity: 0; }
+        }
+        img {
+            width: 400px;
+            height: auto;
+            margin-top: 20px;
+            border: 5px solid red;
+            box-shadow: 0 0 15px red;
+        }
+    </style>
+</head>
+<body>
+    <h1>HACKED BY MR KODEK</h1>
+    <p class="blink">AMATI TIRU MODIVIKASI!</p>
+    <p>种瓜得瓜，种豆得豆。不要怨天尤人，命运在自己手中。</p>
+    <img src="https://cdn.thaibinhtv.vn/upload/banner/file/qorun.png" alt="MR KODEK Logo">
+    <audio autoplay loop>
+        <source src="https://www.myinstants.com/media/sounds/horror.mp3" type="audio/mpeg">
+    </audio>
+</body>
+</html>
